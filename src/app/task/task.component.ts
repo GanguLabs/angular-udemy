88c68b385e182cc4efa02b1895e3e1db2bdef53f
Anwesh Gangula <gangulaanwesh@gmail.com>
2024-07-28 23:36:27 +0530

    fetch task details from data

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  @Input() task!: Task;
 
   constructor() { }
 
